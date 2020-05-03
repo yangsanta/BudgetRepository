729b781fab3389e7e244503a029ee3e9bf5e1cd3
Yang,Ming-Hua <yangsanta77@gmail.com>
2020-05-03 11:05:03 +0800

    BudgetsPerDay 改成 property

--- BudgetApp/BudgetApp/Budget.cs
+++ BudgetApp/BudgetApp/Budget.cs
@@ -21,5 +21,28 @@ namespace BudgetApp
         /// 金額
         /// </summary>
         public int Amount { get; set; }
+
+        public List<BudgetEntity> BudgetsPerDay
+        {
+            get
+            {
+                var budgetsPerDay = new List<BudgetEntity>();
+                var year = int.Parse(this.YearMonth.Substring(0, 4));
+                var month = int.Parse(this.YearMonth.Substring(4, 2));
+                var day = 1;
+                var daysInMonth = DateTime.DaysInMonth(year, month);
+                var amountPerDay = Convert.ToDecimal(this.Amount) / daysInMonth;
+                while (day <= daysInMonth)
+                {
+                    budgetsPerDay.Add(new BudgetEntity()
+                    {
+                        Date = new DateTime(year, month, day++),
+                        Amount = amountPerDay
+                    });
+                }
+
+                return budgetsPerDay;
+            }
+        }
     }
 }
--- BudgetApp/BudgetApp/BugetService.cs
+++ BudgetApp/BudgetApp/BugetService.cs
@@ -17,28 +17,8 @@ namespace BudgetApp
 
         public decimal Query(DateTime startDateTime, DateTime endDateTime)
         {
-            var budgets = this._repo.GetALL();
-            var budgetsPerDay = this.GetBudgetsPerDay(budgets);
+            var budgetsPerDay = this._repo.GetALL().SelectMany(x =>x.BudgetsPerDay);
             return budgetsPerDay.Where(x => startDateTime.Date <= x.Date && x.Date <= endDateTime.Date).Sum(x => x.Amount);
-        }
-
-        private List<BudgetEntity> GetBudgetsPerDay(List<Budget> budgets)
-        {
-            var budgetsPerDay = new List<BudgetEntity>();
-            foreach (var budget in budgets)
-            {
-                var year = int.Parse(budget.YearMonth.Substring(0, 4));
-                var month = int.Parse(budget.YearMonth.Substring(4, 2));
-                var day = 1;
-                var daysInMonth = DateTime.DaysInMonth(year, month);
-                var amountPerDay = Convert.ToDecimal(budget.Amount) / daysInMonth;
-                while (day <= daysInMonth)
-                {
-                    budgetsPerDay.Add(new BudgetEntity() { Date = new DateTime(year, month, day++), Amount = amountPerDay });
-                }
-            }
-
-            return budgetsPerDay;
         }
     }
 }
